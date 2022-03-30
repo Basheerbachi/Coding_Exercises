99b9d948da8eee36c9e5a23ee9b1996e4e18b38a
Basheerbachi <85440600+Basheerbachi@users.noreply.github.com>
2022-03-30 17:23:01 +0530

    Add files via upload

---
+++ Dom_que/script.js
@@ -0,0 +1,45 @@
+// let value=document.getElementById("text")
+
+// value.innerText="Welcome to Prepbytes";
+
+// let a=document.getElementsByTagName("h1")
+
+// a[0].innerText="Competitive coding";
+
+// let b=document.getElementsByClassName("box");
+
+// b[0].innerText="Changed by dom";
+
+// function clickme(){
+//  let c=document.getElementsByTagName("h1")
+//  c[1].innerText="Hello World"
+// }
+
+// function Direction(){
+//  let d=document.getElementsByClassName("container");
+//  d[0].style.flexDirection="column";
+// }
+
+// let e=document.getElementsByTagName("h1")
+
+// e[2].style.color='red';
+// e[2].setAttribute("id","heading");
+
+// function clicked() {
+
+//     var result4=document.getElementById("textchanged");
+//     result4.innerText="Hello World"
+
+// }
+
+// function changedirection(){
+// document.getElementById("boxcontainer").style.flexDirection="column";
+// }
+
+
+// function clickme () {
+//     document.getElementById("heading").style.color="red";
+// } 
+// function replaceme(){
+//     document.getElementById("textchanging").innerHTML="WELCOME TO ELEVATION ACADEMY";
+// }
